fd5672d0eff87738101265d1aa1d249a1c52a123
oisupov <oisupov@brave.com>
2023-06-19 18:45:22 +0700

    Add ipfs.set* for various settings

--- test/data/extensions/api_test/ipfsCompanion/background.js
+++ test/data/extensions/api_test/ipfsCompanion/background.js
@@ -100,6 +100,56 @@ function getSettings(expected_json) {
   })
 }
 
+function setResolveMethod(method) {
+  chrome.ipfs.setResolveMethod(method, (result) => {
+    if (result) {
+      chrome.test.succeed();
+    } else {
+      chrome.test.fail();
+    }
+  })
+}
+
+function setPublicGateway(url) {
+  chrome.ipfs.setPublicGateway(url, (result) => {
+    if (result) {
+      chrome.test.succeed();
+    } else {
+      chrome.test.fail();
+    }
+  })
+}
+
+function setDNSLinkRedirectEnabled(value) {
+  chrome.ipfs.setDNSLinkRedirectEnabled(value, (result) => {
+    if (result) {
+      chrome.test.succeed();
+    } else {
+      chrome.test.fail();
+    }
+  })
+}
+
+function setGatewayResourcesRedirectEnabled(value) {
+  chrome.ipfs.setGatewayResourcesRedirectEnabled(value, (result) => {
+    if (result) {
+      chrome.test.succeed();
+    } else {
+      chrome.test.fail();
+    }
+  })
+}
+
+function setGatewayFallbackEnabled(value) {
+  chrome.ipfs.setGatewayFallbackEnabled(value, (result) => {
+    if (result) {
+      chrome.test.succeed();
+    } else {
+      chrome.test.fail();
+    }
+  })
+}
+
 function testBasics() {
   chrome.test.runTests([
     function ipfsCompanionExtensionHasAccess() {
